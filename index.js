4ada2c7a75fff81da7da6ced49210ca1f4485d4d
Ben Kurose <ben.kurose@gmail.com>
2022-08-08 16:17:58 -0400

    commit tree 954a6f26bb73b06ffe1d0d32087db6206d942479

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue == 42){
+        return "0"
+    }
+    else if (someValue > 42){
+        return someValue - 42
+    }
+    else if (someValue < 42){
+        return 42 - someValue
+    }
+  }
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+  }
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(start - destination) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) > 2500)
+        return 'cannot travel that far'
+    else if (distanceTravelledInFeet(start, destination) > 2000)
+        return 25
+    else if (distanceTravelledInFeet(start, destination) > 400)
+        return (distanceTravelledInFeet(start, destination) -400) * .02
+    else
+        return 0
+  }
